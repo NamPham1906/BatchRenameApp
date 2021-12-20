d5d252f619bef2cecfce73c194f4fc29682a39d1
khoale <kthedestroy@gmail.com>
2021-12-20 17:27:20 +0700

    rule 3

---
+++ RemoveExtraSpaceRule/RuleWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoveExtraSpaceRule
+{
+    /// <summary>
+    /// Interaction logic for RuleWindow.xaml
+    /// </summary>
+    public partial class RuleWindow : UserControl
+    {
+        RemoveExtraSpaceRule rule;
+        public RuleWindow(RemoveExtraSpaceRule rule)
+        {
+            this.rule = rule;
+            InitializeComponent();
+
+        }
+
+        private void LeadingRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            this.rule.Configuration = -1;
+        }
+
+        private void TrailingRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            this.rule.Configuration = 0;
+        }
+
+        private void BothRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            this.rule.Configuration = 1;
+        }
+    }
+}
--- batchRenameApp/MainWindow.xaml.cs
+++ batchRenameApp/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Contract;
 namespace batchRenameApp
 {
     /// <summary>
@@ -19,9 +20,28 @@ namespace batchRenameApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        int ruleAmount = 0;
+        List<IRule> rules = new List<IRule>();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ruleAmount = RuleFactory.GetInstance().RuleAmount();
+            for(int i = 0; i < ruleAmount; i++)
+            {
+                rules.Add(RuleFactory.GetInstance().Create(i));
+            }
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if(ruleAmount > 0)
+        //    {
+        //        MyStackPanel.Children.Add(rules[0].GetUI());
+        //    }
+        //}
     }
 }
