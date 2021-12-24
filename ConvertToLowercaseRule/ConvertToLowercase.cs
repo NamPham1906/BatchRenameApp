0c8a5cfb526f768cbaef704050e1d1dc55c31aa8
HigekiAi <lenguyenthaomi2510@gmail.com>
2021-12-24 14:21:50 +0700

    small bug hmi

--- ConvertToLowercaseRule/ConvertToLowercase.cs
+++ ConvertToLowercaseRule/ConvertToLowercase.cs
@@ -73,7 +73,7 @@ namespace ConvertToLowercaseRule
                     nonex += "." + tokens[i];
 
                 if (type == 1) ex = "." + tokens[tokens.Length - 1];
-                if (type == 2 && tokens.Length > 1) nonex = "." + tokens[tokens.Length - 1];
+                if (type == 2 && tokens.Length > 1) nonex += "." + tokens[tokens.Length - 1];
 
                 string temp = nonex;
 
