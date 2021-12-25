0829ebfe414d1d065a479871d4a7ea5670ad8bb9
khoale <kthedestroy@gmail.com>
2021-12-25 09:45:40 +0700

    remove accent rule

---
+++ RemoveAccentRule/RuleWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RemoveAccentRule
+{
+    /// <summary>
+    /// Interaction logic for RuleWindow.xaml
+    /// </summary>
+    public partial class RuleWindow : UserControl
+    {
+        RemoveAccent rule;
+
+        public RuleWindow(RemoveAccent rule)
+        {
+            this.rule = rule;
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = rule;
+        }
+    }
+}
